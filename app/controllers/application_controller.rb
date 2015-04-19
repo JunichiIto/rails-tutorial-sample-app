a689cdfb673ebf9ea4ad9f6dccc6b0ab59498823
Junichi Ito <jit@sonicgarden.jp>
2015-04-20 07:07:57 +0900

    Add basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_with_basic_auth
   protect_from_forgery with: :exception
   include SessionsHelper
 
@@ -9,6 +10,15 @@ class ApplicationController < ActionController::Base
       store_location
       flash[:danger] = 'Please log in.'
       redirect_to login_url
+    end
+  end
+
+  # Heroku上でのいたずらを防ぐためにBasic認証を付ける
+  def authenticate_with_basic_auth!
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |user, pass|
+        user == ENV['BASIC_AUTH_USER'] && pass == ENV['BASIC_AUTH_PASS']
+      end
     end
   end
 end
