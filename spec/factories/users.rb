b4075980427cc6de9aacb8112c866165406f1f8f
Junichi Ito <jit@sonicgarden.jp>
2015-04-21 08:17:51 +0900

    Add sample controller spec

---
+++ spec/factories/users.rb
@@ -0,0 +1,59 @@
+FactoryGirl.define do
+  factory :user do
+    name Faker::Name.name
+    email { Faker::Internet.email }
+    password 'password'
+    password_confirmation { password }
+    activated true
+    activated_at Time.zone.now
+
+    factory :michael do
+      name 'Michael Example'
+      email 'michael@example.com'
+      admin true
+    end
+
+    factory :archer do
+      name 'Sterling Archer'
+      email 'duchess@example.gov'
+    end
+  end
+end
+
+# michael:
+#   name: Michael Example
+#   email: michael@example.com
+#   password_digest: <%= User.digest('password') %>
+#   admin: true
+#   activated: true
+#   activated_at: <%= Time.zone.now %>
+#
+# archer:
+#   name: Sterling Archer
+#   email: duchess@example.gov
+#   password_digest: <%= User.digest('password') %>
+# activated: true
+# activated_at: <%= Time.zone.now %>
+#
+# lana:
+#   name: Lana Kane
+#   email: hands@example.gov
+#   password_digest: <%= User.digest('password') %>
+#     activated: true
+# activated_at: <%= Time.zone.now %>
+#
+# mallory:
+#   name: Mallory Archer
+#   email: boss@example.gov
+#   password_digest: <%= User.digest('password') %>
+#     activated: true
+# activated_at: <%= Time.zone.now %>
+#
+# <% 30.times do |n| %>
+# user_<%= n %>:
+#     name:  <%= "User #{n}" %>
+#   email: <%= "user-#{n}@example.com" %>
+#     password_digest: <%= User.digest('password') %>
+#   activated: true
+#   activated_at: <%= Time.zone.now %>
+# <% end %>
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,20 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # Configure DatabaseCleaner to reset data between tests
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
