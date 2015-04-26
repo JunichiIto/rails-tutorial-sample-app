2f8ab9a82f442f2f517eeeafaa8852792baedd04
Junichi Ito <jit@sonicgarden.jp>
2015-04-27 07:26:18 +0900

    Add users index spec.

---
+++ spec/features/users_index_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe 'Users index' do
+  before do
+    @admin = FactoryGirl.create :michael
+    @non_admin = FactoryGirl.create :archer
+    FactoryGirl.create_list :user, 30
+  end
+
+  it 'index as admin including pagination and delete links' do
+    log_in_as(@admin)
+    visit users_path
+    expect(page).to have_selector 'h1', text: 'All users'
+    expect(page).to have_selector 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      expect(page).to have_link user.name, href: user_path(user)
+      unless user == @admin
+        expect(page).to have_link 'delete', href: user_path(user)
+        link = find_link 'delete', href: user_path(user)
+        expect(link['data-method']).to eq 'delete'
+      end
+    end
+    expect{click_link 'delete', href: user_path(@non_admin)}.to change{User.count}.by(-1)
+  end
+
+  it 'index as non-admin' do
+    log_in_as(@non_admin)
+    visit users_path
+    expect(page).to_not have_selector 'a', text: 'delete'
+  end
+end
+
+# require 'test_helper'
+#
+# class UsersIndexTest < ActionDispatch::IntegrationTest
+#   def setup
+#     @admin = users(:michael)
+#     @non_admin = users(:archer)
+#   end
+#
+#   test 'index as admin including pagination and delete links' do
+#     log_in_as(@admin)
+#     get users_path
+#     assert_template 'users/index'
+#     assert_select 'div.pagination'
+#     first_page_of_users = User.paginate(page: 1)
+#     first_page_of_users.each do |user|
+#       assert_select 'a[href=?]', user_path(user), text: user.name
+#       unless user == @admin
+#         assert_select 'a[href=?]', user_path(user), text: 'delete', method: :delete
+#       end
+#     end
+#     assert_difference 'User.count', -1 do
+#       delete user_path(@non_admin)
+#     end
+#   end
+#
+#   test 'index as non-admin' do
+#     log_in_as(@non_admin)
+#     get users_path
+#     assert_select 'a', text: 'delete', count: 0
+#   end
+# end
