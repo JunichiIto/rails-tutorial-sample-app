10c27212062d40662134b590cf516f2270a29752
Junichi Ito <jit@sonicgarden.jp>
2015-04-27 19:06:20 +0900

    Add password resets spec.

---
+++ spec/features/password_resets_spec.rb
@@ -0,0 +1,131 @@
+require 'rails_helper'
+
+describe 'Password reset' do
+  before do
+    ActionMailer::Base.deliveries.clear
+    @user = FactoryGirl.create :michael
+  end
+
+  it 'password resets' do
+    visit new_password_reset_path
+    expect(page).to have_selector 'h1', 'Forgot password'
+
+    click_button 'Submit'
+    expect(page).to have_selector '.alert'
+    expect(page).to have_selector 'h1', 'Forgot password'
+
+    fill_in 'Email', with: @user.email
+    click_button 'Submit'
+    expect(@user.reset_digest).to_not eq @user.reload.reset_digest
+    expect(ActionMailer::Base.deliveries.size).to eq 1
+    expect(page).to have_selector '.alert'
+    expect(current_path).to eq root_path
+
+    mail = ActionMailer::Base.deliveries.last
+    reset_token = mail.body.encoded[/(?<=password_resets\/)[^\/]+/]
+
+    visit edit_password_reset_path(reset_token, email: '')
+    expect(current_path).to eq root_path
+
+    @user.toggle!(:activated)
+    visit edit_password_reset_path(reset_token, email: @user.email)
+    expect(current_path).to eq root_path
+    @user.toggle!(:activated)
+
+    visit edit_password_reset_path(reset_token, email: @user.email)
+    expect(page).to have_selector 'h1', 'Reset password'
+    hidden = find '#email'
+    expect(hidden.value).to eq @user.email
+    expect(hidden['type']).to eq 'hidden'
+
+    fill_in 'Password', with: 'foobaz'
+    fill_in 'Confirmation', with: 'barquux'
+    click_button 'Update password'
+    expect(page).to have_selector 'div#error_explanation'
+
+    fill_in 'Password', with: ''
+    fill_in 'Confirmation', with: 'foobar'
+    click_button 'Update password'
+    expect(page).to have_selector '.alert'
+    expect(page).to have_selector 'h1', 'Reset password'
+
+    fill_in 'Password', with: 'foobaz'
+    fill_in 'Confirmation', with: 'foobaz'
+    click_button 'Update password'
+    expect(is_logged_in?).to be_truthy
+    expect(page).to have_selector '.alert'
+    expect(current_path).to eq user_path(@user)
+  end
+end
+
+# require 'test_helper'
+#
+# class PasswordResetsTest < ActionDispatch::IntegrationTest
+#   def setup
+#     ActionMailer::Base.deliveries.clear
+#     @user = users(:michael)
+#   end
+#
+#   test 'password resets' do
+#     get new_password_reset_path
+#     assert_template 'password_resets/new'
+#
+#     post password_resets_path, password_reset: { email: '' }
+#     assert_not flash.empty?
+#     assert_template 'password_resets/new'
+#
+#     post password_resets_path, password_reset: { email: @user.email }
+#     assert_not_equal @user.reset_digest, @user.reload.reset_digest
+#     assert_equal 1, ActionMailer::Base.deliveries.size
+#     assert_not flash.empty?
+#     assert_redirected_to root_url
+#
+#     user = assigns(:user)
+#
+#     get edit_password_reset_path(user.reset_token, email: '')
+#     assert_redirected_to root_url
+#
+#     user.toggle!(:activated)
+#     get edit_password_reset_path(user.reset_token, email: user.email)
+#     assert_redirected_to root_url
+#     user.toggle!(:activated)
+#
+#     get edit_password_reset_path(user.reset_token, email: user.email)
+#     assert_template 'password_resets/edit'
+#     assert_select 'input[name=email][type=hidden][value=?]', user.email
+#
+#     patch password_reset_path(user.reset_token),
+#         email: user.email,
+#         user: { password: 'foobaz', password_confirmation: 'barquux' }
+#     assert_select 'div#error_explanation'
+#
+#     patch password_reset_path(user.reset_token),
+#           email: user.email,
+#           user: { password: '', password_confirmation: 'foobar' }
+#     assert_not flash.empty?
+#     assert_template 'password_resets/edit'
+#
+#     patch password_reset_path(user.reset_token),
+#           email: user.email,
+#           user: { password: 'foobaz', password_confirmation: 'foobaz' }
+#     assert is_logged_in?
+#     assert_not flash.empty?
+#     assert_redirected_to user
+#   end
+#
+#   # See Listing 10.57
+#   test "expired token" do
+#     get new_password_reset_path
+#     post password_resets_path, password_reset: { email: @user.email }
+#
+#     @user = assigns(:user)
+#     @user.update_attribute(:reset_sent_at, 3.hours.ago)
+#     patch password_reset_path(@user.reset_token),
+#           email: @user.email,
+#           user: { password:              "foobar",
+#                   password_confirmation: "foobar" }
+#     assert_response :redirect
+#     follow_redirect!
+#     assert_match /Password reset has expired/i, response.body
+#   end
+# end
