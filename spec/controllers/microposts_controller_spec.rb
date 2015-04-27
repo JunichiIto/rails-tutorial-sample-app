5326653b7191087db90e4ee044901c56a7b793a8
Junichi Ito <jit@sonicgarden.jp>
2015-04-28 06:37:32 +0900

    Add MicropostsController test.

---
+++ spec/controllers/microposts_controller_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe MicropostsController do
+  before do
+    @user = FactoryGirl.create :michael
+    @micropost = FactoryGirl.create :orange, user: @user
+  end
+
+  it 'should redirect create when not logged in' do
+    expect{post :create, micropost: { content: 'Lorem ipsum' }}.to_not change{Micropost.count}
+    expect(response).to redirect_to login_url
+  end
+
+  it 'should redirect destroy when not logged in' do
+    expect{delete :destroy, id: @micropost}.to_not change{Micropost.count}
+    expect(response).to redirect_to login_url
+  end
+
+  it 'should redirect destroy for wrong micropost' do
+    log_in_as(@user)
+    micropost = FactoryGirl.create :ants
+    expect{delete :destroy, id: micropost}.to_not change{Micropost.count}
+    expect(response).to redirect_to root_url
+  end
+end
+
+# require 'test_helper'
+#
+# class MicropostsControllerTest < ActionController::TestCase
+#   def setup
+#     @micropost = microposts(:orange)
+#   end
+#
+#   test 'should redirect create when not logged in' do
+#     assert_no_difference 'Micropost.count' do
+#       post :create, micropost: { content: 'Lorem ipsum' }
+#     end
+#     assert_redirected_to login_url
+#   end
+#
+#   test 'should redirect destroy when not logged in' do
+#     assert_no_difference 'Micropost.count' do
+#       delete :destroy, id: @micropost
+#     end
+#     assert_redirected_to login_url
+#   end
+#
+#   test 'should redirect destroy for wrong micropost' do
+#     log_in_as(users(:michael))
+#     micropost = microposts(:ants)
+#     assert_no_difference 'Micropost.count' do
+#       delete :destroy, id: micropost
+#     end
+#     assert_redirected_to root_url
+#   end
+# end
